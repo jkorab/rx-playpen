43d0c7e0e7f3c8073ad6a47a95910b2dd51806f8
Jakub Korab <jakub.korab@gmail.com>
2014-01-10 20:45:43 +0000

    Added first test :)

---
+++ src/test/java/net/jakubkorab/rx/FirstRxTest.java
@@ -0,0 +1,35 @@
+package net.jakubkorab.rx;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rx.Observable;
+import rx.util.functions.Action1;
+
+import java.util.concurrent.CountDownLatch;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author jkorab
+ */
+public class FirstRxTest {
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void testObservableArray() throws InterruptedException {
+        String[] names = new String[] {"Jake", "Alex"};
+        final CountDownLatch latch = new CountDownLatch(names.length);
+
+        Observable.from(names).subscribe(new Action1<String>() {
+            @Override
+            public void call(String name) {
+               log.info("Hello {}", name);
+               latch.countDown();
+            }
+        });
+
+        latch.await();
+        assertTrue(true);
+    }
+}
